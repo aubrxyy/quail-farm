1da079200cf074a60eda91f608bae9c58dd722fe
Aubrey <141216701+aubrxyy@users.noreply.github.com>
2025-05-06 14:40:05 +0700

    mempercuantik register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,13 @@
 
 import { signup } from '@/app/api/auth/auth'
 import { useState } from 'react'
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+
+const pop = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 export default function Register() {
   const [errors, setErrors] = useState<any>(null)
@@ -13,6 +20,15 @@ export default function Register() {
     setPending(true)
     setErrors(null)
     setSuccess(false)
+
+    const password = (e.currentTarget.elements.namedItem('password') as HTMLInputElement).value;
+    const confirmPassword = (e.currentTarget.elements.namedItem('konfirmpassword') as HTMLInputElement).value;
+
+    if (password !== confirmPassword) {
+      setErrors('Password dan konfirmasi password tidak cocok.');
+      setPending(false);
+      return;
+    }
 
     const formData = new FormData(e.currentTarget)
     const result = await signup(formData)
@@ -26,42 +42,46 @@ export default function Register() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="bg-[#E6DCB8] text-black h-screen flex flex-col items-center justify-center"
-    >
-      <div>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" placeholder="Name" />
+    <div className={`${pop.className} bg-cover bg-center min-h-screen flex items-center justify-center`} style={{ backgroundImage: "url('/loginBG.jpeg')" }}>
+      <div className="bg-white px-8 py-10 rounded-lg shadow-lg text-center w-full max-w-lg">
+        <h2 className={`text-3xl mb-4 text-black font-bold`}>Buat Akun</h2>
+          <>
+            <form id="registerForm" onSubmit={handleSubmit} className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+              <div className="mb-1 sm:col-span-2">
+                <label htmlFor="name" className={`block text-left mb-2 text-gray-700`}>Nama Lengkap</label>
+                <input type="text" id="name" name='name' className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#A0522D] text-black" placeholder="Masukkan Nama Lengkap" required />
+              </div>
+              {/* <div className="mb-1 sm:col-span-2">
+                <label htmlFor="phoneNumber" className={`block text-left mb-2 text-gray-700`}>Nomor Telepon</label>
+                <input type="text" name='phoneNumber' id="phoneNumber" className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#A0522D] text-black" placeholder="Masukkan phone number" required onInput={handlePhoneNumberInput} pattern="[0-9]*" />
+              </div>
+              */}
+              <div className="mb-1 sm:col-span-2">
+                <label htmlFor="email" className={`block text-left mb-2 text-gray-700`}>Email</label>
+                <input type="email" name='email' id="email" className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#A0522D] text-black" placeholder="Masukkan email" required />
+              </div>
+              <div className="mb-1 sm:col-span-2">
+                <label htmlFor="password" className={`block text-left mb-2 text-gray-700`}>Password</label>
+                <input type="password" name='password' id="password" className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#A0522D] text-black" placeholder="Masukkan password" required />
+              </div>
+              <div className="mb-1 sm:col-span-2">
+                <label htmlFor="konfirmpassword" className={`block text-left mb-2 text-gray-700`}>Konfirmasi Password</label>
+                <input type="password" id="konfirmpassword" className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#A0522D] text-black" placeholder="Konfirmasi password" required />
+              </div>
+            <p className={`text-gray-400 mb-1`}>Sudah punya akun? <a href="/login" className="text-[#EDB943] hover:underline">Masuk</a></p>
+              <div className="sm:col-span-2">
+                <button type="submit" className="w-full bg-[#EDB943] hover:bg-[#8B4513] text-white p-2 rounded">Daftar</button>
+              </div>
+            </form>
+
+
+            <hr className="mt-8" />
+            <div className="flex justify-center">
+              <Image src="/logo.png" width={150} height={100} alt="AR3" className="mt-8" />
+            </div>
+          </>
+
       </div>
-      {errors?.name && <p>{errors.name}</p>}
-
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" placeholder="Email" />
-      </div>
-      {errors?.email && <p>{errors.email}</p>}
-
-      <div>
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      {errors?.password && (
-        <div>
-          <p>Password must:</p>
-          <ul>
-            {errors.password.map((error: string) => (
-              <li key={error}>- {error}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {success && <p className="text-green-500">Signup successful!</p>}
-
-      <button disabled={pending} type="submit">
-        {pending ? 'Submitting...' : 'Sign Up'}
-      </button>
-    </form>
+    </div>
   )
 }
